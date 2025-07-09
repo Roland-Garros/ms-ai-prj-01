22b58fdff6a67aa9a71f060fd51e77074ec872ac
DESKTOP-A6QU8H4\KTDS <garnet9469@gmail.com>
2025-07-09 09:18:35 +0900

    [INIT] 프로젝트 생성 및 최초 커밋 * 타 폴더에서 작업하던 프로젝트 관련 파일 사용 * 최초 커밋 수행

---
+++ streamlit.sh
@@ -0,0 +1,3 @@
+#pip install streamlit openai dotenv azure-ai-ml azure-identity azure-ai-projects==1.0.0b12 agents mcp-agent streamlit-option-menu
+pip install streamlit openai dotenv
+python -m streamlit run app_rfp_job_streamlit.py --server.port 8000 --server.address 0.0.0.0
