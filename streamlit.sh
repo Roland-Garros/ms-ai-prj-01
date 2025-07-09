79aa297f95c40225c34757a9d8a055af2e7d89c2
DESKTOP-A6QU8H4\KTDS <garnet9469@gmail.com>
2025-07-09 09:42:14 +0900

    [UPDATE] streamlit 쉘 파일 수정 * pip 패키지 설치 명령어 수정

--- streamlit.sh
+++ streamlit.sh
@@ -1,3 +1,2 @@
-#pip install streamlit openai dotenv azure-ai-ml azure-identity azure-ai-projects==1.0.0b12 agents mcp-agent streamlit-option-menu
-pip install streamlit openai dotenv
+pip install dotenv streamlit openai
 python -m streamlit run app_rfp_job_streamlit.py --server.port 8000 --server.address 0.0.0.0
